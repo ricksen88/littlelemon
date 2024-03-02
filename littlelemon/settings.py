a5972cad6248e9f75b5c29541849a2f0061af894
Alexey Voronkov <contact@webal.dev>
2024-03-02 15:20:03 +0200

    mysql connect

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -77,8 +77,15 @@ WSGI_APPLICATION = 'littlelemon.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'capstone',
+        'USER': 'root',
+        'PASSWORD': '123456',
+        'HOST': '127.0.0.1',
+        'PORT': '1111',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 
--- restaurant/admin.py
+++ restaurant/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from restaurant.models import Bookings, Menu
 
 # Register your models here.
+admin.site.register(Bookings)
+admin.site.register(Menu)
